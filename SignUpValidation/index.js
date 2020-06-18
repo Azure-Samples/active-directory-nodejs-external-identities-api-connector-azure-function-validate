d6e63c44d82bf441c4a41f86bbf42af8509923a9
Edwin Gomez Cuellar <edgomezc@microsoft.com>
2020-06-18 15:53:36 -0400

    fixed angle bracket issue

--- SignUpValidation/index.js
+++ SignUpValidation/index.js
@@ -24,14 +24,14 @@ module.exports = async function (context, req) {
 
   // If input data is null, return error.
   const INVALID_REQUEST = {
-    status: 200,
+    status: 400,
     body: {
       version: API_VERSION,
       code: "INVALID_REQUEST",
     },
   };
 
-  if (!(req.body && req.body.email && req.body.email.contains('@'))) {
+  if (!(req.body && req.body.email && req.body.email.contains("@"))) {
     context.res = INVALID_REQUEST;
     context.log("Invalid Request");
     return;
@@ -64,18 +64,20 @@ module.exports = async function (context, req) {
   }
 
   // Validate the 'Job Title' to ensure it's at least 4 characters.
-  if (req.body.jobTitle) { //use !req.body.jobTitle to require a jobtitle
-   if (req.body.jobTitle.length < 5) {
-    context.res = {
-      status: 400,
-      body: {
-        version: API_VERSION,
-        action: "ValidationError",
+  if (req.body.jobTitle) {
+    //use !req.body.jobTitle to require a jobtitle
+    if (req.body.jobTitle.length < 5) {
+      context.res = {
         status: 400,
-        userMessage: "Please provide a job title with at least 5 characters.",
-        code: "SingUp-Input-Validation-0",
-      },
-    };
+        body: {
+          version: API_VERSION,
+          action: "ValidationError",
+          status: 400,
+          userMessage: "Please provide a job title with at least 5 characters.",
+          code: "SingUp-Input-Validation-0",
+        },
+      };
+    }
   }
 
   // Email domain and user collected attribute are valid, return continuation response.
